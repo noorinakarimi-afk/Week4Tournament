4248b695987ecf3af9c894ea52d67c906704dbe5
noorinakarimi-afk <noorina.karimi@stu.ocsb.ca>
2026-09-21 11:14:24 -0400

    Teams names and scores

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 #############################################
-# Name: Your name
+# Name: Noorina Karimi
 # Class: ICS3C
 # Date: Friday Sept. 25
 # Project Name: Week4Tournament
@@ -8,3 +8,80 @@
 #############################################
 
 # THIS IS WHERE YOU CODE
+print("Welcome to Week4Tournament")
+#Read in team 1 name
+print("What is team 1? ")
+team1 = input()
+#Read in team 1 wins
+print("How many wins does team 1 have? ")
+wins1 = int(input())
+#Read in team 1 losses
+print("How many losses does team 1 have? ")
+losses1 = int(input())
+#Read in team 1 ties
+print("How many ties does team 1 have? ")
+ties1 = int(input())
+#Read in team 2 name
+print("What is team 2? ")
+team2 = input()
+#Read in team 2 wins
+print("How many wins does team  2 have? ")
+wins2 = int(input())
+#Read in team 2 losses
+print("How many losses does team  2 have? ")
+losses2 = int(input())
+#Read in team 2 ties
+print("How many ties does team  2 have? ")
+ties2 = int(input())
+#Read in team 3 name
+print("What is team 3? ")
+team3 = input()
+#Read in team 3 wins
+print("How many wins does team  3 have? ")
+wins3 = int(input())
+#Read in team 3 losses
+print("How many losses does team  3 have? ")
+losses3 = int(input())
+#Read in team 3 ties
+print("How many ties does team  3 have? ")
+ties3 = int(input())
+#Read in team 4 name
+print("What is team 4? ")
+team4 = input()
+#Read in team 4 wins
+print("How many wins does team  4 have? ")
+wins4 = int(input())
+#Read in team 4 losses
+print("How many losses does team  4 have? ")
+losses4 = int(input())
+#Read in team 4 ties
+print("How many ties does team  4 have? ")
+ties4 = int(input())
+#Read in team 5 name
+print("What is team 5? ")
+team5 = input()
+#Read in team 5 wins
+print("How many wins does team  5 have? ")
+wins5 = int(input())
+#Read in team 5 losses
+print("How many losses does team  5 have? ")
+losses5 = int(input())
+#Read in team 5 ties
+print("How many ties does team  5 have? ")
+ties5 = int(input())
+#Read in team 6 name
+print("What is team 6? ")
+team6 = input()
+#Read in team 6 wins
+print("How many wins does team  6 have? ")
+wins6 = int(input())
+#Read in team 6 losses
+print("How many losses does team  6 have? ")
+losses6 = int(input())
+#Read in team 6 ties
+print("How many ties does team  6 have? ")
+ties6 = int(input())
+#Calculate the total points
+totalp = wins1*2+ties1*1
+print("Total wins: "+str(totalp))
+
