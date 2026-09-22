f77edbce9a899b67a2ade478c2875b78622d5a4e
noorinakarimi-afk <noorina.karimi@stu.ocsb.ca>
2026-09-22 11:12:27 -0400

    Complete tournament project

--- Main.py
+++ Main.py
@@ -81,7 +81,44 @@ losses6 = int(input())
 #Read in team 6 ties
 print("How many ties does team  6 have? ")
 ties6 = int(input())
-#Calculate the total points
-totalp = wins1*2+ties1*1
-print("Total wins: "+str(totalp))
-
+#Calculate the total points for team 1
+totalp1 = wins1*2+ties1*1
+print("Team Name:",team1,"Wins:",wins1,"Losses:",losses1,"Ties:",ties1,"Total Points: "+str(totalp1))
+#Calculate the total points for team 2
+totalp2 = wins2*2+ties2*1
+print("Team Name:",team2,"Wins:",wins2,"Losses:",losses2,"Ties:",ties2,"Total points: "+str(totalp2))
+#Calculate the total points for team 3
+totalp3 = wins3*2+ties3*1
+print("Team Name:",team3,"Wins:",wins3,"Losses:",losses3,"Ties:",ties3,"Total points: "+str(totalp3))
+#Calculate the total points for team 4
+totalp4 = wins4*2+ties4*1
+print("Team Name:",team4,"Wins:",wins4,"Losses:",losses4,"Ties:",ties4,"Total points: "+str(totalp4))
+#Calculate the total points for team 5
+totalp5 = wins5*2+ties5*1
+print("Team Name:",team5,"Wins:",wins5,"Losses:",losses5,"Ties:",ties5,"Total points: "+str(totalp5))
+#Calculate the total points for team 6
+totalp6 = wins6*2+ties6*1
+print("Team Name:",team6,"Wins:",wins6,"Losses:",losses6,"Ties:",ties6,"Total points: "+str(totalp6))
+#Find the team with the highest score
+top_team_name = ""
+max_points = -1
+if totalp1 > max_points:
+    max_points = totalp1
+    top_team_name = team1
+if totalp2 > max_points:
+    max_points = totalp2
+    top_team_name = team2
+if totalp3 > max_points:
+    max_points = totalp3
+    top_team_name = team3
+if totalp4 > max_points:
+    max_points = totalp4
+    top_team_name = team4
+if totalp5 > max_points:
+    max_points = totalp5
+    top_team_name = team5
+if totalp6 > max_points:
+    max_points_name = totalp6
+    top_team = team6
+#Top team
+print("Top team:",top_team_name,"is at the top of the standings. Congratulations to",top_team_name,"team!")
